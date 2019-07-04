f5288c92da7b81b1208bee2e3eaa9e155e34b44e
m.az94@hotmail.com <maria@marias-MacBook-Air.local>
2019-07-04 18:05:59 -0400

    created APIs on client-side code

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,8 +1,21 @@
 import axios from "axios";
-import cheerio from "cheerio";
+// import cheerio from "cheerio";
 
-// export default {
-//     getTopsFromASOS: 
-//     }
-// }
+export default {
+    findTops: function(){
+        return axios.get("/scrape/tops");
+    },
+    findBottoms: function (){
+        return axios.get("/scrape/bottoms");
+    },
+    findDresses: function(){
+        return axios.get("/scrape/dresses");
+    },
+    findShoes: function(){
+        return axios.get("/scrape/shoes");
+    },
+    findAccessories: function(){
+        return axios.get("/scrape/accessories");
+    }
+}
 
