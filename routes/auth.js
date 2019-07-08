a89cf267749529c54bde65b5d67d4f5f8c044273
Nirossan <46507846+Nirossan@users.noreply.github.com>
2019-07-08 00:31:17 -0400

    Update auth.js
    updated Radio button

--- routes/auth.js
+++ routes/auth.js
@@ -17,7 +17,7 @@ router.post('/register', function(req, res) {
         name:req.body.name,
         email: req.body.email,
         password: req.body.password,
-        gender: req.body.gender
+        type: req.body.type
       });
       // save the user
       newUser.save(function(err) {
