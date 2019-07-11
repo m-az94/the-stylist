ee5f9877aaa1bec194f1fe008f570d9ced157e37
Phuoc phan <phuocphan12@gmail.com>
2019-07-11 02:54:08 -0400

    fixed merge

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -3,7 +3,6 @@ import "./style.css";
 
 function Card(props) {
     return (
-<<<<<<< HEAD
         <div
             className="card"
 >
@@ -12,9 +11,6 @@ function Card(props) {
             <h5 class="card-title">{props.name}</h5>
         </div>
         
-=======
-        <div className="card" style={{backgroundImage: props.image ? `url(${props.image})` : "none"}}>
->>>>>>> master
         </div>
     );
 }
--- models/clientStylistRef.js
+++ models/clientStylistRef.js
@@ -5,11 +5,7 @@ const clientStylistRef = new Schema({
   clientID: { type: String, required: true },
   clientName: { type: String, required: true},
   stylistID: { type: String, required: true },
-<<<<<<< HEAD
   styleResult: Object,
-=======
-  styleResult: [],
->>>>>>> master
   hotOrNot: Boolean,
   date: { type: Date, default: Date.now }
 });
